a1bc5b451531bbbf9bbc411f086e019384fa37da
Rutvik1803 <rutu1803@gmail.com>
2024-01-04 11:43:28 +0530

    Initial Commit

---
+++ src/router/router.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SignIn from '../pages/SignIn';
+import Home from '../pages/Home';
+import Profile from '../pages/Profile';
+import SignUp from '../pages/SignUp';
+
+
+
+const Router = () => {
+  return (
+      <Routes>
+        <Route path='/' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+  );
+};
+
+export default Router;
