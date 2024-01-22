afd0e49fdac72c73ef162707e25111d284360f99
Rutvik1803 <rutu1803@gmail.com>
2024-01-22 15:54:46 +0530

    added redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from '@mui/material'
 import theme from './styles/theme.js'
-import {store} from './redux/store.js'
+import {persistor, store} from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null}>
     <ThemeProvider theme={theme}>
     <App />
     </ThemeProvider>
+    </PersistGate>
   </Provider>,
 )
